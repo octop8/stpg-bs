9ed9a3f67715432ff8e5d6860eba92b15e1dbfd8
kamikal <kamithepenguin@gmail.com>
2016-09-18 20:16:31 +0800

    added temp

---
+++ temp/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#tab').click(function() {
+    $('#otherbox').css('background-image', 'url(src/images/fullsize/tab.png)');
+  });
+  $('#tab2').click(function() {
+    $('#otherbox').css('background-image', 'url(src/images/fullsize/tab2.png)');
+  });
+  $('#tab3').click(function() {
+    $('#otherbox').css('background-image', 'url(src/images/fullsize/tab3.png)');
+  });
+});
